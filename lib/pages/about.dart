b2d69be9b5557b99a95f78a980d841ce2f4a7b68
Savirrrr <savirp1905@gmail.com>
2024-08-12 13:46:45 +0530

    Changes to about page and added events page

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,68 +1,87 @@
 import 'package:flutter/material.dart';
 import 'app_bar.dart'; // Import the custom AppBar
 
-class AboutPage extends StatelessWidget {
+class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
+
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  double _appBarOpacity = 1.0;
+
+  void _onMenuSelected(String value) {
+    switch (value) {
+      case 'Home':
+        Navigator.pushReplacementNamed(context, '/home');
+        break;
+      case 'About':
+        Navigator.pushReplacementNamed(context, '/about');
+        break;
+      case 'Events':
+        Navigator.pushReplacementNamed(context, '/events');
+        break;
+      case 'Contact Us':
+        Navigator.pushReplacementNamed(context, '/contact');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 37, 40, 82),
       appBar: CustomAppBar(
-        opacity: 1.0, // Full opacity for the AppBar
-        onMenuSelected: (value) {
-          // Handle menu selection, navigating to appropriate screens
-          switch (value) {
-            case 'Home':
-              Navigator.pushReplacementNamed(context, '/home');
-              break;
-            case 'About':
-              Navigator.pushReplacementNamed(context, '/about');
-              break;
-            case 'Events':
-              Navigator.pushReplacementNamed(context, '/events');
-              break;
-            case 'Contact Us':
-              Navigator.pushReplacementNamed(context, '/contact');
-              break;
-          }
-        },
+        opacity: _appBarOpacity,
+        onMenuSelected: _onMenuSelected,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification notification) {
+          if (notification is ScrollUpdateNotification) {
+            setState(() {
+              _appBarOpacity =
+                  (1.0 - notification.metrics.pixels / 200).clamp(0.0, 1.0);
+            });
+          }
+          return true;
+        },
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Achievements',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Achievements',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Our technical club has excelled in local, regional, and national hackathons, highlighting our members\' strong problem-solving abilities and coding expertise. Our workshops and coding boot camps are well-attended, receiving positive feedback and emphasizing our dedication to knowledge sharing. Additionally, our members have published research in respected journals and conferences, showcasing our commitment to technological innovation and community impact.',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
+                const SizedBox(height: 10),
+                const Text(
+                  'Our technical club has excelled in local, regional, and national hackathons, highlighting our members\' strong problem-solving abilities and coding expertise. Our workshops and coding boot camps are well-attended, receiving positive feedback and emphasizing our dedication to knowledge sharing. Additionally, our members have published research in respected journals and conferences, showcasing our commitment to technological innovation and community impact.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  _buildStatCard(
-                      'assets/images/zoom_logo.png', '10+', 'Events'),
-                  _buildStatCard(
-                      'assets/images/people_logo.png', '1000+', 'Students'),
-                  _buildStatCard(
-                      'assets/images/medal_logo.png', '26', 'Awards'),
-                ],
-              ),
-            ],
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _buildStatCard(
+                        'assets/images/zoom_logo.png', '10+', 'Events'),
+                    _buildStatCard(
+                        'assets/images/people_logo.png', '1000+', 'Students'),
+                    _buildStatCard(
+                        'assets/images/medal_logo.png', '26', 'Awards'),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/events.dart
+++ lib/pages/events.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
+import 'app_bar.dart';
 
-class EventsPage extends StatelessWidget {
+class EventsPage extends StatefulWidget {
   const EventsPage({super.key});
+
+  @override
+  State<EventsPage> createState() => _EventsPageState();
+}
+
+class _EventsPageState extends State<EventsPage> {
+  double _appBArOpacity = 1.0;
+
+  void _onMenuSelected(String value) {
+    switch (value) {
+      case 'Home':
+        Navigator.pushReplacementNamed(context, '/home');
+        break;
+      case 'About':
+        Navigator.pushReplacementNamed(context, "/about");
+        break;
+      case 'Events':
+        Navigator.pushReplacementNamed(context, "/events");
+        break;
+      case 'Contact Us':
+        Navigator.pushReplacementNamed(context, "/contact");
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Events'),
+      backgroundColor: const Color.fromARGB(255, 37, 40, 82),
+      appBar: CustomAppBar(
+        opacity: _appBArOpacity,
+        onMenuSelected: _onMenuSelected,
       ),
-      body: const Center(
-        child: Text('This is the Events page'),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification notification) {
+          if (notification is ScrollUpdateNotification) {
+            setState(() {
+              _appBArOpacity =
+                  (1.0 - notification.metrics.pixels / 200).clamp(0.0, 1.0);
+            });
+          }
+          return true;
+        },
+        child: SingleChildScrollView(),
       ),
     );
   }
